3eee470e9d061b8a34189633699cb6b256891ce3
sonofoven <novesen@calpoly.edu>
2025-12-27 18:04:32 -0800

    VPC, Subnets, and EKS added to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,13 +30,13 @@ data "aws_iam_policy_document" "cluster_assume_role"{
   }
 }
 
-resource "aws_iam_role" "cluster_role"{
+resource "aws_iam_role" "cluster"{
   name = var.cluster_role_name
   description = "Enables eks cluster to be in auto mode"
   assume_role_policy = data.aws_iam_policy_document.cluster_assume_role
 }
 
-resource "aws_iam_role_policy_attachment" "attachments" {
+resource "aws_iam_role_policy_attachment" "cluster_attachments" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy",
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
@@ -45,7 +45,7 @@ resource "aws_iam_role_policy_attachment" "attachments" {
     "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",
   ])
 
-  role       = aws_iam_role.cluster_role.name
+  role       = aws_iam_role.cluster.name
   policy_arn = each.value
 }
 
@@ -62,20 +62,109 @@ data "aws_iam_policy_document" "worker_node_assume_role"{
   }
 }
 
-resource "aws_iam_role" "worker_node_role"{
+resource "aws_iam_role" "worker_node"{
   name = var.cluster_role_name
   description = "Enables ec2 node to be part of eks cluster & use ecr"
   assume_role_policy = data.aws_iam_policy_document.worker_node_assume_role
 }
 
-resource "aws_iam_role_policy_attachment" "attachments" {
+resource "aws_iam_role_policy_attachment" "worker_node_attachments" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
   ])
 
-  role       = aws_iam_role.worker_node_role.name
+  role       = aws_iam_role.worker_node.name
   policy_arn = each.value
+}
+
+## Create ECR
+
+resource "aws_ecr_repository" "nginx-texsite" {
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+## Create necessary networking environment for eks
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_subnet" "az3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}c"
+}
+
+## Create EKS 
+
+resource "aws_eks_cluster" "nginx-texsite" {
+  name = var.eks_cluster_name
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.31"
+
+  bootstrap_self_managed_addons = false
+
+  compute_config {
+    enabled       = true
+    node_pools    = ["general-purpose"]
+    node_role_arn = aws_iam_role.worker_node.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.az1.id,
+      aws_subnet.az2.id,
+      aws_subnet.az3.id,
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_attachments,
+    aws_iam_role_policy_attachment.worker_node_attachments,
+
+  ]
 }
 
